b8e7460624af19e55a725698ccc07c25fe3ad8d2
unknown <sadorusek@gmail.com>
2022-04-30 19:05:12 +0200

    Cypress Setup

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -13,9 +13,9 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+import '@percy/cypress'
 // Import commands.js using ES2015 syntax:
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-import '@percy/cypress'
