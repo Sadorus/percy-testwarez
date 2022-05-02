3c0ae950e0060a01b443727f39848b9562b7d0f9
unknown <sadorusek@gmail.com>
2022-05-02 12:40:39 +0200

    Content + Percy Tests

--- cypress/integration/visual/blog.spec.js
+++ cypress/integration/visual/blog.spec.js
@@ -1,12 +1,27 @@
 describe('PercyLuczniczQA', function() {
-    beforeEach(function() {
+    before(function() {
       // Load our app before starting each test case
-      cy.visit('localhost:1313')
+      cy.visit('/')
     })
   
-    it('Loads the Content', function() {
+    it('Loads the Blog Content', function() {
       cy.get('.main').should('exist')
-      cy.percySnapshot()
+      cy.percySnapshot('Homepage')
+    })
+
+    it('Move to the second page', function() {
+      cy.get('a.next').click()
+      cy.percySnapshot('Blog Page')
+    })
+
+    it('Lorem Ipsum Post Content', function() {
+      cy.visit('/posts/lorem-ipsum/')
+      cy.percySnapshot('Lorem Ipsum Blog Post')
+    })
+
+    it('Example Blog Post', function() {
+      cy.visit('/posts/cool-post/')
+      cy.percySnapshot('Styling')
     })
   })
 
