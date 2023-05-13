6aba3a897854a1059614f0337460c7d8ddeff92a
Mateusz Bloch <mateuszbloch90@gmail.com>
2023-05-13 22:17:16 +0200

    next fix

--- cypress/e2e/visual/blog.cy.js
+++ cypress/e2e/visual/blog.cy.js
@@ -1,19 +1,19 @@
 describe('PercyLuczniczQA', () => {
-    before(() => {
-      cy.visit('/')
-    })
-  
+
     it('Loads the Blog Content',() => {
+      cy.visit('/')
       cy.get('.main').should('exist')
       cy.percySnapshot('Homepage', { widths: [768, 992, 1200] })
     })
 
     it('Loads the Blog Content and make a snapshot of navigation',() => {
+      cy.visit('/')
       cy.get('.main').should('exist')
       cy.percySnapshot('Navigation', { widths: [768, 992, 1200] }, {percyCSS: 'nav.nav'})
     })
 
     it('Move to the second page',() => {
+      cy.visit('/')
       cy.get('a.next').click()
       cy.percySnapshot('Blog Page')
     })
